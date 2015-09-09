76691c2fe10c719c0686b4ba7271ef8b4802ba59
misnyo <misnyo@misnyo.eu>
2015-09-09 22:39:01 +0200

    [enh]optparse added

--- opice.rb
+++ opice.rb
@@ -2,8 +2,29 @@ require 'rubygems'
 require 'bundler/setup'
 require 'aws-sdk'
 require 'yaml'
+require 'optparse'
 
 CONFIG = YAML.load_file("config/config.yaml") unless defined? CONFIG
+available_commands = ["list", "start", "stop"]
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: opice.rb command [options]"
+  opts.separator ""
+  opts.separator "Available commands: " + available_commands.join(", ")
+  opts.separator ""
+
+  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options[:verbose] = v
+  end
+  opts.on_tail("-h", "--help", "Show this message") do
+    puts opts
+    exit
+  end
+end.parse!
+command = ARGV.pop
+raise "Need to specify a command" unless command
+raise "Wrong command, try -h" unless available_commands.include? command
 
 ec2 = Aws::EC2::Client.new(region: CONFIG['awsconfig']['AWS_REGION'], 
            credentials: Aws::Credentials.new(
