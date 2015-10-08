6a4e6d074aa1e407ad4e5aed723c925415f58368
misnyo <misnyo@misnyo.eu>
2015-10-08 14:44:05 +0200

    [enh]puppet script for creating lb env added

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,13 @@ FROM ubuntu:14.04
 RUN \
     apt-get update && \
     apt-get -y upgrade && \
-    apt-get install -y ruby git ruby-bundler
+    apt-get install -y ruby git ruby-bundler puppet
 
 ADD dacp.rb /root/dacp/dacp.rb
 ADD config /root/dacp/config
 ADD Gemfile /root/dacp/Gemfile
 ADD .aws /root/.aws
+ADD puppet /root/dacp/puppet
 
 ENV HOME /root
 WORKDIR /root
@@ -15,6 +16,7 @@ WORKDIR /root
 RUN \
     cd /root/dacp && \
     bundle install && \
+    puppet module install puppetlabs-aws && \
     ./dacp.rb
 
 CMD ["bash"]
---
+++ puppet/create.pp
@@ -0,0 +1,89 @@
+Ec2_securitygroup {
+  region => 'ap-southeast-1',
+}
+
+Ec2_instance {
+  region            => 'ap-southeast-1',
+  availability_zone => 'ap-southeast-1b',
+}
+
+Elb_loadbalancer {
+  region => 'ap-southeast-1',
+}
+
+ec2_securitygroup { 'lb-sg':
+  ensure      => present,
+  description => 'Security group for load balancer',
+  ingress     => [{
+    protocol => 'tcp',
+    port     => 80,
+    cidr     => '0.0.0.0/0'
+  }],
+}
+
+ec2_securitygroup { 'web-sg':
+  ensure      => present,
+  description => 'Security group for web servers',
+  ingress     => [{
+    security_group => 'lb-sg',
+  },{
+    protocol => 'tcp',
+    port     => 22,
+    cidr     => '0.0.0.0/0'
+  }],
+}
+
+ec2_securitygroup { 'db-sg':
+  ensure      => present,
+  description => 'Security group for database servers',
+  ingress     => [{
+    security_group => 'web-sg',
+  },{
+    protocol => 'tcp',
+    port     => 22,
+    cidr     => '0.0.0.0/0'
+  }],
+}
+
+ec2_instance { ['web-1', 'web-2']:
+  ensure          => present,
+  image_id        => 'ami-8c1607de',
+  security_groups => ['web-sg'],
+  instance_type   => 't2.micro',
+  tags            => {
+    department => 'engineering',
+    project    => 'cloud',
+    created_by => $::id,
+  }
+}
+
+ec2_instance { 'db-1':
+  ensure          => present,
+  image_id        => 'ami-8c1607de',
+  security_groups => ['db-sg'],
+  instance_type   => 't2.micro',
+  monitoring      => true,
+  tags            => {
+    department => 'engineering',
+    project    => 'cloud',
+    created_by => $::id,
+  },
+  block_devices => [
+    {
+      device_name => '/dev/sda1',
+      volume_size => 8,
+    }
+  ]
+}
+
+elb_loadbalancer { 'lb-1':
+  ensure             => present,
+  availability_zones => ['ap-southeast-1b'],
+  instances          => ['web-1', 'web-2'],
+  listeners          => [{
+    protocol           => 'tcp',
+    load_balancer_port => 80,
+    instance_protocol  => 'tcp',
+    instance_port      => 80,
+  }],
+}
